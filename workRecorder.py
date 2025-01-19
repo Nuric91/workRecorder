aa6654a8f85924d4fdfa6192621a189888fb6f60
Costa Weiland <nuric91@gmail.com>
2025-01-19 15:08:47 +0100

    added name to copyright disclaimer

--- workRecorder.py
+++ workRecorder.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025. All rights reserved.
+# Copyright (c) 2025 by Costa Weiland. All rights reserved.
 # This software is provided "as-is," without any express or implied warranty.
 # In no event will the authors be held liable for any damages arising from the use of this software.
 # Permission is granted to anyone to use this software for any purpose, 
